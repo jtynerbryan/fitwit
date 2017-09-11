03d300a28c6e5af797e3e1da28ed0c11b9c5c492
shomiyata <eighteighttwenty@gmail.com>
2017-09-11 11:56:11 -0400

    added event listeners on planForm

--- src/components/PlanForm.js
+++ src/components/PlanForm.js
@@ -6,48 +6,75 @@ class PlanForm extends React.Component {
 		super()
 
 		this.state = {
-			backBiceps: [],
-			shoulderAbs: [],
-			chestTriceps: [],
-			legsCalves: [],
-			daysAWeek: 0,
-			programLength: 0
+			backBiceps: false,
+			shoulderAbs: false,
+			chestTriceps: false,
+			legsCalves: false,
+			daysAWeek: undefined,
+			programLength: undefined
 		}
 	}
 
 	handleSubmit = (event) => {
 		event.preventDefault()
-	}
 
-	handleDaysAWeek = () => {
 
 	}
 
-	handleProgramLength = () => {
-
+	handleDaysAWeek = (event) => {
+		this.setState({
+			daysAWeek: parseInt(event.target.value)
+		})
 	}
 
-	handleCheckbox = () => {
+	handleProgramLength = (event) => {
+		this.setState({
+			programLength: parseInt(event.target.value)
+		})
+	}
+
+	handleCheckbox = (event) => {
+		console.log(event.target.innerHTML)
+		if (event.target.innerHTML === 'Back/biceps'){
+			this.setState({
+				backBiceps: !this.state.backBiceps
+			},()=>console.log(this.state))
+		}
+		else if (event.target.innerHTML === 'Shoulder/abs'){
+			this.setState({
+				shoulderAbs: !this.state.shoulderAbs
+			},()=>console.log(this.state))
+		}
+		else if (event.target.innerHTML === 'Chest/triceps'){
+			this.setState({
+				chestTriceps: !this.state.chestTriceps
+			},()=>console.log(this.state))
+		}
+		else if (event.target.innerHTML === 'Legs/calves'){
+			this.setState({
+				legsCalves: !this.state.legsCalves
+			},()=>console.log(this.state))
+		}
 
 	}
 
 	render() {
 		return (
-		  <Form>
+		  <Form onSubmit={this.handleSubmit}>
 		    <Form.Field>
 		      <label>How many days a week would you like to work out?</label>
-		      <input placeholder='Please enter 1-4' />
+		      <input type='number' min='1' max='4' placeholder='Please enter 1-4' onChange={this.handleDaysAWeek} value={this.state.daysAWeek}/>
 		    </Form.Field>
 		    <Form.Field>
 		      <label>How many weeks do you want your program to run for?</label>
-		      <input placeholder='Please enter 4-12' />
+		      <input type='number' min='4' max='12' placeholder='Please enter 4-12' onChange={this.handleProgramLength} value={this.state.programLength} />
 		    </Form.Field>
 		    <Form.Field>
 		    <label>If you're working out less than 4 days a week, please select the muscle groups you would like to work on</label>
-		      <Checkbox label='Back/biceps' />
-		      <Checkbox label='Shoulder/abs' />
-		      <Checkbox label='Chest/triceps' />
-		      <Checkbox label='Legs/calves' />
+		      <Checkbox label='Back/biceps' name='backBiceps' onChange={this.handleCheckbox} /><br/>
+		      <Checkbox label='Shoulder/abs' onChange={this.handleCheckbox} /><br/>
+		      <Checkbox label='Chest/triceps' onChange={this.handleCheckbox} /><br/>
+		      <Checkbox label='Legs/calves' onChange={this.handleCheckbox} />
 		    </Form.Field>
 		    <Button type='submit'>Submit</Button>
 		  </Form>
