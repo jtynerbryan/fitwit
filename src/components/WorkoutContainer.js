2be077d21d4ccaf420ffd5c9681c9e79532c304b
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-09-12 20:58:00 -0400

    able to retrieve a user's exercises from back-end

--- src/components/WorkoutContainer.js
+++ src/components/WorkoutContainer.js
@@ -12,30 +12,37 @@ class WorkoutContainer extends React.Component {
 
 	componentDidMount = () => {
 			const usernameJSON = JSON.stringify({username: this.props.username})
-			fetch('http://localhost:3001/api/v1/exercises',{
-			method: 'get',
-			body: usernameJSON
-		})
+		    return fetch('http://localhost:3001/api/v1/exercises',{
+		      method: 'get',
+		      headers: {
+		        "Content-Type":"application/json",
+		        "Accept":"application/json",
+		        "username":usernameJSON
+		      }
+		    })
 			.then(res => res.json())
-			.then(res => this.setState({ exercises: res })
+			.then(res => console.log(res))
 	}
 
-	const workoutContainers = () => {
-		const allContainers = []
-		for(let i = 1; i <= this.props.plan.programLength; i++){
-			allContainers.push(<WeekContainer key={i} week={i} exercises={this.props.exercises} plan={this.props.plan} />)
-		}
-		return allContainers
+	// const workoutContainers = () => {
+	// 	const allContainers = []
+	// 	for(let i = 1; i <= this.props.plan.programLength; i++){
+	// 		allContainers.push(<WeekContainer key={i} week={i} exercises={this.props.exercises} plan={this.props.plan} />)
+	// 	}
+	// 	return allContainers
+	// }
+
+	// {workoutContainers().map(container => container)}
+
+	
+	render() {
+		return (
+			<div>
+				<h1>{this.props.username}'s Workout Plan</h1>
+			</div>
+		)		
 	}
 
-	{workoutContainers().map(container => container)}
-
-	return(
-		<div>
-			<h1>{this.props.username}'s Workout Plan</h1>
-
-		</div>
-	)
 }
 
 export default WorkoutContainer
